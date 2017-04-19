5439a337fb649c10e5ca54786efa8b6165aee848
Saule BN <beyonce@thinkful.com>
2017-04-19 14:46:02 -0400

    modified

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
 const {ShoppingList} = require('./models');
+const {Recipes} = require('./models');
 
 const jsonParser = bodyParser.json();
 const app = express();
@@ -17,6 +18,7 @@ app.use(morgan('common'));
 ShoppingList.create('beans', 2);
 ShoppingList.create('tomatoes', 3);
 ShoppingList.create('peppers', 4);
+Recipes.create('chocolate milk', ['cocoa', 'milk', 'sugar']);
 
 // when the root of this router is called with GET, return
 // all current ShoppingList items
@@ -24,7 +26,30 @@ app.get('/shopping-list', (req, res) => {
   res.json(ShoppingList.get());
 });
 
+//GET All Recipes
+app.get('/recipes', (req, res) => {
+  res.json({message:'getallrecipes'});
+});
+//GET SPECIFIC RECIPES
+app.get('/recipes/:id', (req, res) => {
+  res.json({message:`getaspecificrecipes ${req.params.id}`});
+});
+//CREATE RECIPES
+app.post('/recipes', (req, res) => {
+  res.json({message:'createrecipes'});
+});
+app.post('/recipes/:chocolate milk'), (req, res) => {
+  res.json({message:'chocolate milk'});
+});
+
+
+//Recipes.create('chocolate milk', ['cocoa', 'milk', 'sugar'])
+//DELETE RECIPES
+app.delete('/recipes/:id', (req, res) => {
+  res.json({message:'deleterecipes'});
+});
+
+
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
 });
-//Hello World!!!
